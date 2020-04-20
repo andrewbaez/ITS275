6b14a97340be317155b40f230e03b165a143880a
andrewbaez <57456167+andrewbaez@users.noreply.github.com>
2020-04-20 19:28:13 -0400

    final version

--- Test2Sub.java
+++ Test2Sub.java
@@ -1,17 +1,6 @@
 package finalProject;
 
 public class Test2Sub implements Test2 {
-//	  public void animalSound() {
-//	    System.out.println("The cat implements interfaces saying: meow!!! ");
-//	  }
-//	  public void sleep() {
-//	    System.out.println("the cat implements interfaces by taking a nap");
-//	  }
-//
-//	public void eat() {
-//	    System.out.println("the cat implements interfaces by eating");
-//		
-//	}
 	@Override
 	public void Quarentine() {
 		// TODO Auto-generated method stub
